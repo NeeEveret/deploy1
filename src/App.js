789638fad9db8738202028e6030ff38295863f02
neeeveret <darkveidery0.5@gmail.com>
2025-09-27 00:01:42 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLA ME GUSTA EL DINERO<br/>
+          noooooooooooooooooooooooo<br/>
           Edit <code>src/App.js</code> and save to reload.
         
         </p>
